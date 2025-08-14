717a02a27b26f495e9ad078b0cab1cc637537703
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-08-14 13:14:06 +0530

    Update authController.js

--- project/server/controller/authController.js
+++ project/server/controller/authController.js
@@ -28,8 +28,9 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false
+   sameSite: "None",
+secure: true
+
     });
 console.log("Token set in cookie:", token);
     return res.status(201).json(user);
